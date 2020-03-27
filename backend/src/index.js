dcc578e4a838e94ea13ee50811fe9d61ae12acc9
Jefferson <jefferson.car@hotmail.com>
2020-03-26 23:06:46 -0300

    Mobile da aplicacao

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,11 +28,13 @@
 * Query Builder: table('users').select('*').where() (KNEX)
 */
 
+const cors = require('cors')
 const express = require('express')
 const routes = require('./routes')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
